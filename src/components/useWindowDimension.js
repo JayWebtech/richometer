e17d8b4a9ac38a5a1206c20834f2ed9dfd782731
Adamu Jethro <jethroadamzy@gmail.com>
2024-09-21 13:48:07 +0100

    bhuhu

--- src/components/useWindowDimension.js
+++ src/components/useWindowDimension.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 
 const useWindowDimensions = () => {
   const [windowDimensions, setWindowDimensions] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0,
   });
 
   useEffect(() => {
@@ -14,8 +14,15 @@ const useWindowDimensions = () => {
       });
     };
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleResize);
+    }
+
+    return () => {
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handleResize);
+      }
+    };
   }, []);
 
   return windowDimensions;
